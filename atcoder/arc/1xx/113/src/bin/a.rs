b536354c8519c6ec8ea13d2eb5206fb305100a1c
sueki <ueki.popngfdmddr@gmail.com>
2023-02-10 09:16:21 +0900

    solve arc113-a

---
+++ atcoder/arc/1xx/113/src/bin/a.rs
@@ -0,0 +1,47 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, zip, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use proconio::marker::{Chars, Usize1};
+#[allow(unused_imports)]
+use proconio::{input, source::line::LineSource};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+#[allow(unused_imports)]
+use std::io::BufRead;
+
+fn solve(k: i64, rest: i64) -> i64 {
+    if rest == 1 {
+        k
+    } else {
+        (1..=k).map(|i| solve(k / i, rest - 1)).sum::<i64>()
+    }
+}
+
+fn main() {
+    input! {
+        k: i64
+    };
+
+    println!("{}", solve(k, 3));
+}
