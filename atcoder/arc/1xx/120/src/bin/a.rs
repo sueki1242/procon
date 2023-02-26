d32b62e2672864a95d2a29b759f3b37c0ad9dfb9
sueki <ueki.popngfdmddr@gmail.com>
2023-02-26 22:09:17 +0900

    solve arc120-a

---
+++ atcoder/arc/1xx/120/src/bin/a.rs
@@ -0,0 +1,47 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
+use std::io;
+#[allow(unused_imports)]
+use std::iter::*;
+#[allow(unused_imports)]
+use std::mem::*;
+#[allow(unused_imports)]
+use std::str::*;
+#[allow(unused_imports)]
+use std::usize;
+
+#[allow(unused_imports)]
+use bitset_fixed::BitSet;
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, iterate, izip, zip, Itertools};
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use proconio::marker::{Chars, Usize1};
+#[allow(unused_imports)]
+use proconio::{input, source::line::LineSource};
+#[allow(unused_imports)]
+use rustc_hash::FxHashMap;
+#[allow(unused_imports)]
+use rustc_hash::FxHashSet;
+#[allow(unused_imports)]
+use std::io::BufRead;
+
+fn main() {
+    input! {
+        n: i64,
+        a: [i64; n]
+    };
+    let mut sum = 0;
+    let mut cum = 0;
+    let mut ma = 0;
+    for (i, &v) in a.iter().enumerate() {
+        ma = max(ma, v);
+        sum += v;
+        cum += sum;
+        println!("{}", cum + ma * ((i + 1) as i64));
+    }
+}
